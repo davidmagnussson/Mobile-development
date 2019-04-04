2ebf2d4de9e2aaa0c366c71dd5b32f945984aa33
davidmagnussson <davidmagnusson97@gmail.com>
2019-04-05 01:26:47 +0200

    test

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-console.log("main js works");
-
-if ('serviceWorker' in navigator) {  // Är service worker aktiverat???
-    navigator.serviceWorker.register('/service-worker.js')
-    .then(function () {console.log("serviceWorker is ookok");});
-}
+console.log("test");
